75f3c22625fa356ccc7a39378072f2fa0efd5cda
Prasad Viduranga <pkpviduranga@gmail.com>
2021-06-22 17:52:06 +0530

    Create Anchor pane for find option

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,4 +1,44 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
 public class EditorFormController {
+    public TextArea txtEditor;
+    public TextField txtSearch;
+    public AnchorPane pneFind;
+
+
+
+    public void mnuItemNew_OnAction(ActionEvent actionEvent) {
+        txtEditor.clear();
+        txtEditor.requestFocus();
+    }
+
+    public void mnuItemExit_OnAction(ActionEvent actionEvent) {
+
+        if(!txtEditor.getText().isEmpty()){
+            new Alert(Alert.AlertType.CONFIRMATION,"Are you sure?", ButtonType.YES).show();
+
+        }
+    }
+
+    public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
+        if(!txtEditor.getText().isEmpty()){
+            txtEditor.selectAll();
+        }
+    }
+
+    public void btnFindUp_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFindDown_OnAction(ActionEvent actionEvent) {
+    }
 }
